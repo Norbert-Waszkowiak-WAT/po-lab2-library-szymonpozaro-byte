58caffe60e7bde05204364d41e671740b38b613f
szymonpozaro-byte <szymon.pozaro@gmail.com>
2025-12-10 19:28:45 +0000

    67

--- library.cpp
+++ library.cpp
@@ -4,38 +4,44 @@
 #include "book.cpp"
 using namespace std;
 class Library {
-    private:
-    Book**books;
-    int count;
-    public:
-    Library():count(0), books(nullptr){}
-    void addBook(Book* book){
-        count++;
-        Book** newBooks=new Book*[count];
-        newBooks[count-1] = book;
-        for(int i=0; i<count-1; i++){
-            newBooks[i]=books[i];
-        }
-        delete[] books;
-        books = newBooks;
+private:
+Book** books;
+int count;
+public:
+Library(): count(0), books(nullptr){}
+void addBook(Book* book){
+    count++;
+    Book** newBooks = new Book*[count];
+    newBooks[count-1] = book;
+    for(int i = 0; i < count-1; i++){
+        newBooks[i] = books[i];
     }
-    void removeBook(int index){
-        delete books[index];
-        for(int i=index; i<count-1; index++){
-            books[i]=books [i+1];
-        }
-        count--;
+    delete[] books;
+    books = newBooks;
+}
+void removeBook(int index){
+    if(index >= count || index < 0) {
+        cout<<"Invalid index"<<endl;
+        return;
     }
-    Book**getBooks(){
-        return books;
+    delete books[index];
+    for(int i = index; i < count-1; i ++){
+        books[i] = books [i+1];
     }
-    int getCount(){
-        return count;
+    count--;
+}
+Book** getBooks(){
+    return books;
+}
+int getCount() {
+    return count;
+}
+~Library(){
+    for (int i = 0; i < count; i++){
+        delete books[i];
     }
-    ~Library(){
-        for(int i=0; i<count; i++)
-        delete[] books;
-    }
+    delete[] books;
+    cout<<"Library destroyed"<<endl;
+}
 };
 #endif
-
